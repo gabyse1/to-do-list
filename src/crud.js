b4c01f468decaa5d5eccb6f0df88ae7d4a6f3ee3
ASUS <Gaby Snchez>
2022-02-11 20:26:09 -0500

    Add theresyncTask and  updateOrderTask functions which will be called by event listeners

--- src/crud.js
+++ src/crud.js
@@ -1,7 +1,10 @@
 import * as LS from './localStorage.js';
+const today = new Date();
 
 const addTask = (arr, task) => {
-  arr.push({ id: arr.length + 1, description: task, completed: false });
+  arr.push({
+    id: arr.length + 1, description: task, completed: false, date: today.getTime(),
+  });
   LS.saveLocalStorage(arr);
 };
 
@@ -27,6 +30,25 @@ const updateStatusTask = (arr, taskId, status) => {
   LS.saveLocalStorage(arr);
 };
 
+const updateOrderTask = (arr, newOrderIds) => {
+  const newArr = [];
+  newOrderIds.forEach((id, index) => {
+    arr[id - 1].id = index + 1;
+    newArr.push(arr[id - 1]);
+  });
+  LS.saveLocalStorage(newArr);
+};
+
+const resyncTask = (arr) => {
+  const taskIds = [];
+  arr.forEach((ele) => {
+    const dateTask = new Date(ele.date).getTime();
+    const dateToday = today.getTime();
+    if ((dateToday - dateTask) > 3600000) taskIds.push(ele.id);
+  });
+  dropTask(arr, taskIds);
+};
+
 export {
-  addTask, dropTask, editTask, updateStatusTask,
+  addTask, dropTask, editTask, updateStatusTask, updateOrderTask, resyncTask,
 };
